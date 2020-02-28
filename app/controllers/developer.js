3eeaf60439e8593e05a350ca8cf5dea943e4f71b
ixxvivxxi <ixxvivxxi@yahoo.com>
2020-02-28 21:18:55 +0300

    add check for existing role or framework

--- app/controllers/developer.js
+++ app/controllers/developer.js
@@ -11,7 +11,6 @@ export default class DeveloperController extends Controller {
   async save() {
     if (!this.model.developer.firstName) {
       return this.set('errors.firstName', true);
-     
     }
     await this.model.developer.save();
     this.transitionToRoute('index');
@@ -19,14 +18,38 @@ export default class DeveloperController extends Controller {
 
   @action
   async createRole(name) {
+    if (
+      this.model.roles
+        .map(role => role.name.toLowerCase())
+        .includes(name.toLowerCase())
+    ) {
+      return;
+    }
+
     const role = this.store.createRecord('role', { name });
     await role.save();
+
+    this.model.developer.set('role', role);
   }
 
   @action
   async createFramework(name) {
+    if (
+      this.model.frameworks
+        .map(framework => framework.name.toLowerCase())
+        .includes(name.toLowerCase())
+    ) {
+      return;
+    }
+
     const framework = this.store.createRecord('framework', { name });
     await framework.save();
+
+    const developerFrameworks = this.model.developer.get('frameworks');
+    this.model.developer.set(
+      'frameworks',
+      developerFrameworks.addObject(framework)
+    );
   }
 
   @action
